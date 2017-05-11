8b8f66775365ef7dbacb93e7ab96109534903a30
Thomas A. McGonagle <mcgonagle@gmail.com>
2017-05-11 06:39:26 -0400

    made some changes

--- run_ansible.sh
+++ run_ansible.sh
@@ -6,6 +6,7 @@ while test $# -gt 0; do
                         echo " "
                         echo "options:"
                         echo "-h, --help                show brief help"
+                        echo "-a, --all                 run the site playbook"
                         echo "-n, --onboarding          run the onboarding playbook"
                         echo "-o, --operation           run the operation playbook"
                         echo "-t, --teardown            run the teardown playbook"
@@ -13,27 +14,35 @@ while test $# -gt 0; do
                         ;;
                 -n)
                         shift
-                        ansible-playbook onboarding.yml --ask-vault-pass -e @password.yml -vvv 
+                        ansible-playbook playbooks/onboarding.yml --ask-vault-pass -e @password.yml -vvv 
                         shift
                         ;;
                 --onboarding*)
-                        ansible-playbook onboarding.yml --ask-vault-pass -e @password.yml -vvv 
+                        ansible-playbook playbooks/onboarding.yml --ask-vault-pass -e @password.yml -vvv 
                         shift
                         ;;
                 -o)
-                        ansible-playbook operations.yml --ask-vault-pass -e @password.yml -vvv 
+                        ansible-playbook playbooks/operations.yml --ask-vault-pass -e @password.yml -vvv 
                         shift
                         ;;
                 --operation*)
-                        ansible-playbook operations.yml --ask-vault-pass -e @password.yml -vvv 
+                        ansible-playbook playbooks/operations.yml --ask-vault-pass -e @password.yml -vvv 
                         shift
                         ;;
                 -t)
-                        ansible-playbook teardown.yml --ask-vault-pass -e @password.yml -vvv 
+                        ansible-playbook playbooks/teardown.yml --ask-vault-pass -e @password.yml -vvv 
                         shift
                         ;;
                 --teardown*)
-                        ansible-playbook teardown.yml --ask-vault-pass -e @password.yml -vvv 
+                        ansible-playbook playbooks/teardown.yml --ask-vault-pass -e @password.yml -vvv 
+                        shift
+                        ;;
+                -a)
+                        ansible-playbook site.yml --ask-vault-pass -e @password.yml -vvv 
+                        shift
+                        ;;
+                --all*)
+                        ansible-playbook site.yml --ask-vault-pass -e @password.yml -vvv 
                         shift
                         ;;
                 *)
